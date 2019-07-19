7a61dac11ec62eb5a33f219029bdf6bd25f61543
Sami <puggsoy@gmail.com>
2019-07-19 21:33:57 +1200

    Basic scene-switching gameplay

---
+++ Assets/Scripts/TitleScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleScript : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+	
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+		{
+			SceneManager.LoadScene("Game", LoadSceneMode.Single);
+		}
+    }
+}
