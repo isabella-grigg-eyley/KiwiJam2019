fd627012e8a8833693501d9f202b144e33198a20
Sami <puggsoy@gmail.com>
2019-07-20 13:16:59 +1200

    Add carriage settings

---
+++ Assets/Scripts/CarriageSettings.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEditor;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+	[CreateAssetMenu]
+	class CarriageSettings : ScriptableObject
+	{
+		public enum Color
+		{
+			Red,
+			Green,
+			Blue,
+			Wild,
+			Choose,
+		}
+
+		public enum Ability
+		{
+			None,
+			Swap,
+			Give,
+		}
+
+		[SerializeField]
+		private Color m_color = Color.Red;
+
+		[SerializeField]
+		private Ability m_ability = Ability.None;
+	}
+}
